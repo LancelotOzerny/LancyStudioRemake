0c144d5f66d706c31bdd4d74b29fb793f6009368
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-09-05 14:30:58 +0300

    Реализовал паттерн проектирования singleton

---
+++ app/modules/system/traits/singleton.php
@@ -0,0 +1,20 @@
+<?php
+namespace Modules\System\Traits;
+
+trait Singleton
+{
+    protected static self | null $instance = null;
+
+    protected function __construct() {}
+    protected function __clone() {}
+
+    public static function Instance() : self | null
+    {
+        if (self::$instance instanceof self === false)
+        {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+}
