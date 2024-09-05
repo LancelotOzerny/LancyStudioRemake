5b0e23afe5ec88721b280497bf4189eeb3578dc0
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-09-05 14:31:27 +0300

    Написал основной класс приложения

---
+++ app/modules/system/classes/application.php
@@ -0,0 +1,45 @@
+<?php
+namespace Modules\System\Classes;
+
+use Modules\System\Traits\Singleton;
+
+class Application
+{
+    use Singleton;
+
+    private string $current_uri = '';
+    private array $params = [];
+
+    public function init() : void
+    {
+        $arr = explode('?', $_SERVER['REQUEST_URI']);
+        $this->current_uri = $arr[0];
+
+        if (isset($arr[1]))
+        {
+            $this->setParams($arr[1]);
+        }
+    }
+
+    private function setParams(string $params) : void
+    {
+        $this->params = [];
+        $paramsList = explode('&', $params);
+
+        foreach ($paramsList as $param)
+        {
+            $arr = explode('=', $param);
+            $this->params[$arr[0]] = $arr[1] ?? null;
+        }
+    }
+
+    public function getUri() : string
+    {
+        return $this->current_uri;
+    }
+
+    public function getParams() : array
+    {
+        return $this->params;
+    }
+}
