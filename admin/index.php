73611157463da270348b70e7cca1dda9bf08b5f3
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-09-06 17:32:36 +0300

    Добавил индексную административную страницу

---
+++ admin/index.php
@@ -0,0 +1,11 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/app/header.php';
+?>
+<div class="container">
+    <div class="page-block">
+        <div class="page-title">Административная страница</div>
+    </div>
+</div>
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/app/footer.php';
+?>
