cedab1c7cf098e976e2929c1552b46ce0f45975f
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-09-06 16:53:52 +0300

    Внес правки в 404 файл

--- 404.php
+++ 404.php
@@ -1,9 +1,14 @@
-<?php
-require_once 'header.php';
-?>
+<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/app/header.php'; ?>
 
-<h2>Page 404</h2>
+<div class="container" style="min-height: 40vh">
+	<div class="page-block">
+		<div class="page-title">Ошибка 404</div>
+		<div class="wrapper">
+			<p>Уважаемый пользователь!</p>
+			<p>Вы попали на страницу, которая не существует.</p>
+			<p>Попробуйте перейти на другую страницу, к примеру из главного меню.</p>
+		</div>
+	</div>
+</div>
 
-<?php
-require_once 'footer.php';
-?>
+<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/app/footer.php'; ?>
