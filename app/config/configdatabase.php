4912182f2f56a647b0339cbae91e052db041086c
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-09-07 23:27:47 +0300

    Добавил настройки БД

---
+++ app/config/configdatabase.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Config;
+
+class ConfigDatabase
+{
+    public static string $host = 'localhost';
+    public static string $dataBase = 'database';
+    public static string $user = 'root';
+    public static string $password = '';
+}
