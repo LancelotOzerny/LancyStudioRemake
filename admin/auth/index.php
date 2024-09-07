499a5f23763f158142512bcbe2c3bb30daecff04
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-09-07 20:19:32 +0300

    Реализовал обращение к документу и передачу параметров через post

--- admin/auth/index.php
+++ admin/auth/index.php
@@ -1,6 +1,26 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/app/header.php';
+
+use App\Modules\System\Classes\Application;
+
 /** @var string $templatePath */
+
+if (Application::Instance()->request->post->has('userLogin'))
+{
+	$data = [];
+
+	$name = Application::Instance()->request->post->get('userLogin');
+	$pass = Application::Instance()->request->post->get('userPass');
+
+	$name = trim(htmlspecialchars($name));
+	$pass = trim(htmlspecialchars($pass));
+
+	$hashPass = password_hash($pass, PASSWORD_DEFAULT);
+
+	// Обработка данных
+
+	die(json_encode($data));
+}
 
 \App\Modules\System\Classes\Template::Instance()->includeHeader();
 ?>
@@ -9,7 +29,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/app/header.php';
         <div class="page-title" style="text-align: center; display: block">Авторизация</div>
 
         <div class="display-grid grid-justify--around">
-			<form method="post" class="form form--bordered">
+			<form id="AuthForm" class="form form--bordered">
 				<div class="form-block">
 					<label for="inputLogin">Логин</label>
 					<input class="form-input" name="user-name" type="text" id="inputLogin" placeholder="Логин">
@@ -17,16 +37,42 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/app/header.php';
 
 				<div class="form-block">
 					<label for="inputPassword">Пароль</label>
-					<input class="form-input" name="user-pass" type="password" id="inputPassword" placeholder="Пароль">
+					<input class="form-input" name="user-pass" type="password" id="inputPassword" value="test" placeholder="Пароль">
 				</div>
 
 				<div class="display-grid grid-justify--around">
-					<input class="form-input form-input--rounded form-send" type="submit" value="Войти">
+					<input id="sendButton" class="form-input form-input--rounded form-send" type="submit" value="Войти">
 				</div>
 			</form>
 		</div>
     </div>
 </div>
+
+<script>
+	document.addEventListener('jqueryLoaded', () => {
+		$('.form.ajax').ready(function()
+        {
+        	$('#AuthForm').submit(function(e) {
+                e.preventDefault();
+
+                let form = $(this);
+
+                $.ajax({
+					type: 'post',
+					url: '<?= \App\Modules\System\Classes\Application::Instance()->getUri() ?>',
+					data: {
+                        userLogin: form.find('#inputLogin').val(),
+                        userPass: form.find('#inputPassword').val(),
+					},
+					success: function(data)
+					{
+                        console.info(data)
+					}
+				});
+			});
+        });
+	});
+</script>
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/app/footer.php';
 ?>
