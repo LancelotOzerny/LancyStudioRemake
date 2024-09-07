38a7159eac2a45fcbda034c7bc72916e68f70338
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-09-08 00:43:46 +0300

    Добавил таблицы прав и пользователей

---
+++ develop/classes/database/tables/userstable.php
@@ -0,0 +1,9 @@
+<?php
+namespace Develop\Classes\Database\Tables;
+
+use App\Modules\Database\Classes\Table;
+
+class UsersTable extends Table
+{
+    protected static string $tableName = 'Users';
+}
