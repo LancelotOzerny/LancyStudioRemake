538f021dd76785509d51326810de1c5ede8b5547
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-09-08 07:06:32 +0300

    Убрал подключение jQuery

--- develop/templates/default/footer.php
+++ develop/templates/default/footer.php
@@ -52,7 +52,6 @@
 	</div>
 </div>
 
-<script async src="<?= $this->templatePath ?>/assets/scripts/jquery-3.7.1.min.js" onload="document.dispatchEvent(new Event('jqueryLoaded'))"></script>
 <script src="<?= $this->templatePath ?>/assets/scripts/carousel.js"></script>
 <script>
     document.addEventListener('DOMContentLoaded', function(event) {
