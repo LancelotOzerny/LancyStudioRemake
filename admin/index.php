6256b2a5350f66c9d840b1a9805bf335063b9adf
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-09-08 07:41:28 +0300

    Добавил стили для заголовка административной страницы

--- admin/index.php
+++ admin/index.php
@@ -4,9 +4,9 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/app/header.php';
 
 \App\Modules\System\Classes\Template::Instance()->includeHeader();
 ?>
-<div class="container">
+<div class="container" style="padding-left: 40px">
     <div class="page-block">
-        <div class="page-title">Административная страница</div>
+        <div class="page-title page-title--center page-title--padding-25">Административная страница</div>
     </div>
 </div>
 <?php
