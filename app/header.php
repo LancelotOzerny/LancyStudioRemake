b90bbf5f81ca8ca251a1be7a2aa04382cc42c201
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-09-08 03:37:27 +0300

    Инициализировал пользователя в header

--- app/header.php
+++ app/header.php
@@ -6,7 +6,14 @@ use App\Modules\System\Classes\Template;
 
 try
 {
+    session_start();
     Application::Instance()->init();
+    $currentUser = new \App\Modules\System\Classes\User();
+
+    if (Application::Instance()->request->session->has('login-user'))
+    {
+        $currentUser->loadByLogin(Application::Instance()->request->session->get('login-user'));
+    }
 
     Template::Instance()->load(Application::Instance()->getUri());
     $templatePath = Template::Instance()->templatePath;
