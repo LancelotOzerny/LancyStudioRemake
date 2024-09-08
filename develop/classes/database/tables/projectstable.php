8d868d950f341e1d5498891fdb093e70927f97bb
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-09-08 16:20:21 +0300

    Добавил таблицы проектов и типов проектов

---
+++ develop/classes/database/tables/projectstable.php
@@ -0,0 +1,9 @@
+<?php
+namespace Develop\Classes\Database\Tables;
+
+use App\Modules\Database\Classes\Table;
+
+class ProjectsTable extends Table
+{
+    protected static string $tableName = 'Projects';
+}
---
+++ develop/classes/database/tables/projectstypetable.php
@@ -0,0 +1,9 @@
+<?php
+namespace Develop\Classes\Database\Tables;
+
+use App\Modules\Database\Classes\Table;
+
+class ProjectsTypeTable extends Table
+{
+    protected static string $tableName = 'ProjectsType';
+}
