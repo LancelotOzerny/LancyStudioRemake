38e0accf9c3b0083a5a609d4934813409913bbc8
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-09-08 08:52:28 +0300

    Добавил страницы админки

---
+++ admin/settings/users/index.php
@@ -0,0 +1,14 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/app/header.php';
+/** @var string $templatePath */
+
+\App\Modules\System\Classes\Template::Instance()->includeHeader();
+?>
+<div class="container" style="padding-left: 40px">
+    <div class="page-block">
+        <div class="page-title page-title--center page-title--padding-25">Список пользователей</div>
+    </div>
+</div>
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/app/footer.php';
+?>
